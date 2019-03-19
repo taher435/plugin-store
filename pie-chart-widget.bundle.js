809a2d56a3dcfb0b7bd2c1d53d06a40420064636
Taher Dhilawala <taher435@gmail.com>
2019-03-19 18:40:27 +0530

    Add pie chart plugin component rolled up file

---
+++ pie-chart-widget.bundle.js
@@ -0,0 +1,85 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core'), require('base-widget')) :
+	typeof define === 'function' && define.amd ? define(['exports', '@angular/core', 'base-widget'], factory) :
+	(factory((global['plugin-a'] = {}),global.core,global.baseWidget));
+}(this, (function (exports,core,baseWidget) { 'use strict';
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+
+
+
+
+
+
+function __decorate(decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+}
+
+
+
+function __metadata(metadataKey, metadataValue) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(metadataKey, metadataValue);
+}
+
+var PieChartWidgetComponent = /** @class */ (function () {
+    function PieChartWidgetComponent() {
+    }
+    PieChartWidgetComponent.prototype.ngOnInit = function () {
+    };
+    PieChartWidgetComponent = __decorate([
+        core.Component({
+            selector: 'gc-pie-chart-widget',
+            template: "\n    <p>\n      pie-chart-widget works!\n    </p>\n  ",
+            styles: []
+        }),
+        __metadata("design:paramtypes", [])
+    ], PieChartWidgetComponent);
+    return PieChartWidgetComponent;
+}());
+
+var PieChartWidgetModule = /** @class */ (function () {
+    function PieChartWidgetModule() {
+    }
+    PieChartWidgetModule = __decorate([
+        core.NgModule({
+            imports: [
+                baseWidget.BaseWidgetModule
+            ],
+            providers: [{
+                    provide: 'widgets',
+                    useValue: [{
+                            name: 'pie-chart-widget',
+                            component: PieChartWidgetComponent
+                        }]
+                }],
+            declarations: [PieChartWidgetComponent],
+            // exports: [PieChartWidgetComponent],
+            entryComponents: [PieChartWidgetComponent]
+        })
+    ], PieChartWidgetModule);
+    return PieChartWidgetModule;
+}());
+
+exports.PieChartWidgetModule = PieChartWidgetModule;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
